c9e89e58fc77dc7caa214fc7e6560d4112a70780
nhardiansa <nhardiansa@gmail.com>
2021-10-20 14:17:59 +0800

    learn how to use off canvas and text optimize

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles/main.css';
+
+const hamburgerIcon = document.querySelector('#hamburger')
+const drawerNavigationList = document.querySelector('.nav__list ')
+
+hamburgerIcon.addEventListener('click', () => {
+  drawerNavigationList.classList.toggle('clicked')
+})
